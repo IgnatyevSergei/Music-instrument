b3d6df8f698c8207bac685495a02897b7fb4c3d8
AfanasenkoYulka <afanasenkoyulka94@gmail.com>
2022-02-21 20:33:41 +0300

    add 123

--- newScript.js
+++ newScript.js
@@ -4,9 +4,22 @@ const playSound = (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
   if (!audio) return;
-  button.classList.add("transform");
+  button.classList.add("play");
   audio.currentTime = 0;
   audio.play();
-
 };
-window.addEventListener("keydown", playSound)
+
+const removeTransition = (e) => {
+  // if (e.propertyName !== "transform") return;
+
+  console.log(e);
+
+  e.target.classList.remove("play");
+};
+
+const keys = document.querySelectorAll(".notes");
+
+keys.forEach((el) =>
+  el.addEventListener("webkitTransitionEnd", () => console.log("privet"))
+);
+window.addEventListener("keydown", playSound);
