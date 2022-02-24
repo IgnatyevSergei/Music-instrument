038a6baaed965c25fc3269be0801580e3699506b
AfanasenkoYulka <afanasenkoyulka94@gmail.com>
2022-02-24 19:26:20 +0300

    complit

--- newScript.js
+++ newScript.js
@@ -12,8 +12,7 @@ const playSound = (e) => {
 const removeTransition = (e) => {
   if (e.propertyName !== "transform") return;
 
-  console.log(e);
-
+  
   e.target.classList.remove("play");
 };
 
@@ -24,3 +23,16 @@ keys.forEach((el) =>
 );
 
 window.addEventListener("keydown", playSound);
+
+
+const playClickSound = (e) =>{
+
+  const audio = document.querySelector(`audio[data-key="${e.target.getAttribute("data-key")}"]`);
+
+  if (!audio) return;
+  e.target.classList.add("play");
+  audio.currentTime = 0;
+  audio.play();
+};
+
+keys.forEach((el) => el.addEventListener("click", playClickSound));
